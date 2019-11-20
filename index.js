4684cb3a45ee64e6f66d3c7cb77d159e64ad56da
Bayu Fanani <fanani707@gmail.com>
2019-11-21 02:01:39 +0700

    update port to follow standart

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 //create a server object:
+var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(port); //the server object listens on port 8080
