b9b7a60d12bd74c2117b90691e3125d6efef9a48
Bayu Fanani <fanani707@gmail.com>
2019-11-21 03:03:03 +0700

    original

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var http = require('http');
 
 //create a server object:
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  }); //write a response to the client
-  res.end('Hello World!'); //end the response
-}); //the server object listens on port 8080
-server.listen();
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(); //the server object listens on port 8080
