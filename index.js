23e81e39d8ee4952bf7c7497a794e4d5af09eb53
Bayu Fanani <fanani707@gmail.com>
2019-11-21 02:50:53 +0700

    add head
    and also mode write to end

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  }); //write a response to the client
+  res.end('Hello World!'); //end the response
 }).listen(); //the server object listens on port 8080
