dbd87dccc1241392389924ba5ff1eefb8f748ada
Bayu Fanani <fanani707@gmail.com>
2019-11-21 02:09:44 +0700

    show port

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(port); //the server object listens on port 8080
+console.log(port);
