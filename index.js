90ee6cf394455b550a98a86be14a2536dc0b4774
Bayu Fanani <fanani707@gmail.com>
2019-11-21 02:34:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var http = require('http');
 
 //create a server object:
-var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(port); //the server object listens on port 8080
-console.log(port);
+}).listen(); //the server object listens on port 8080
